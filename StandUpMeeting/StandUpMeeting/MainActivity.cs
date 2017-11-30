6699f2664371ceff14fddf90dc956aa9f12db411
Nathan Spaulding <nathan.spaulding@travelport.com>
2017-11-29 20:23:12 -0700

    Navigational Drawer Added

--- StandUpMeeting/StandUpMeeting/MainActivity.cs
+++ StandUpMeeting/StandUpMeeting/MainActivity.cs
@@ -1,18 +1,82 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using SupportToolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.V7.App;
+using Android.Support.V4.Widget;
+using Android.Views;
+using Android.Content.Res;
 
 namespace StandUpMeeting
 {
-    [Activity(Label = "StandUpMeeting", MainLauncher = true)]
-    public class MainActivity : Activity
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
     {
+        private SupportToolbar mToolbar;
+        private MyActionBarDrawerToggle mDrawerToggle;
+        private DrawerLayout mDrawerLayout;
+        private ListView mDrawerList;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            mToolbar = FindViewById<SupportToolbar>(Resource.Id.toolbar);
+            mDrawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            mDrawerList = FindViewById<ListView>(Resource.Id.drawer);
+
+            SetSupportActionBar(mToolbar);
+
+            mDrawerToggle = new MyActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                Resource.String.openDrawer,
+                Resource.String.closeDrawer
+            );
+         
+            mDrawerLayout.AddDrawerListener(mDrawerToggle);
+            SupportActionBar.SetHomeButtonEnabled(true);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            SupportActionBar.SetDisplayShowTitleEnabled(true);
+            mDrawerToggle.SyncState();
+
+            if (savedInstanceState != null)
+            {
+                if (savedInstanceState.GetString("DrawerState") == "Opened")
+                    SupportActionBar.SetTitle(Resource.String.openDrawer);
+                else if (savedInstanceState.GetString("DrawerState") == "Closed")
+                    SupportActionBar.SetTitle(Resource.String.closeDrawer);
+            }
+            else
+            {
+                //This is the first time the activity is ran
+                SupportActionBar.SetTitle(Resource.String.app_name);
+            }
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            mDrawerToggle.OnOptionsItemSelected(item);
+            return base.OnOptionsItemSelected(item);
+        }
+
+        protected override void OnPostCreate(Bundle savedInstanceState)
+        {
+            base.OnPostCreate(savedInstanceState);
+            mDrawerToggle.SyncState();
+        }
+
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            if (mDrawerLayout.IsDrawerOpen((int)GravityFlags.Left))
+                outState.PutString("DrawerState", "Opened");
+            else
+                outState.PutString("DrawerState", "Closed");
+
+            base.OnSaveInstanceState(outState);
         }
     }
 }
